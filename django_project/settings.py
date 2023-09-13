cc80b1c75f09218bfc0cefca8c3225df07c22c1c
dimitarhristov86 <dimitar_hristov86@abv.bg>
2023-09-13 20:23:56 +0300

    edit settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = ['https://owngaragebg-7331cd8fad38.herokuapp.com/', ]
+ALLOWED_HOSTS = ['https://owngaragebg-7331cd8fad38.herokuapp.com/']
 
 
 # Application definition
