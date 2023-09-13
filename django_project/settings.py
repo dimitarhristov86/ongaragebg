16289cc03d20b35fdfbda5bbcd7ad299678a946a
dimitarhristov86 <dimitar_hristov86@abv.bg>
2023-09-13 21:17:28 +0300

    edit settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["owngaragebg-a7b168b4a6e8.herokuapp.com"]
 
 
 # Application definition
