ce8e4539cec73770d515de5ac638ccd54ed36e37
dimitarhristov86 <dimitar_hristov86@abv.bg>
2023-09-13 21:01:51 +0300

    Initial commit

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = ['https://owngaragebg-7331cd8fad38.herokuapp.com/']
+ALLOWED_HOSTS = []
 
 
 # Application definition
